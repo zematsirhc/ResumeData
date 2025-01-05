a51b30da4131b24cd9beef8e56f4122651c6a76c
zematsirhc <123906157+zematsirhc@users.noreply.github.com>
2025-01-05 17:46:43 -0600

    Commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import polars as pl
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.pipeline import Pipeline
 from datetime import datetime
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OneHotEncoder
@@ -11,6 +10,10 @@ training = pl.read_parquet(r"border_crossings\data\train_set.parquet")
 
 
 class DateTransformer(BaseEstimator, TransformerMixin):
+    """
+    Transforms a string date column into a proper date format
+    """
+
     def __init__(self, column: str, date_format="%b %Y"):
         self.date_format = date_format
         self.column = column
@@ -26,19 +29,24 @@ class DateTransformer(BaseEstimator, TransformerMixin):
 
         if self.column not in X.columns:
             raise ValueError(f"Column '{self.column}' not found in the DataFrame.")
-        X = X.with_columns(pl.col(self.column).str.to_datetime("%b %Y"))
+        X = X.with_columns(pl.col(self.column).str.to_date("%b %Y"))
         epoch = pl.lit(datetime(1950, 1, 1))
+        # Calculate days since epoch
+        nanoseconds_per_day = 24 * 60 * 60 * 1_000_000_000  # Nanoseconds in a day
         X = X.with_columns(
-            (pl.col(self.column) - epoch).cast(pl.Int64).alias("Days Since Epoch")
+            ((pl.col(self.column) - epoch).cast(pl.Int64) / nanoseconds_per_day)
+            .cast(pl.Int64)
+            .alias("Days Since Epoch")
         )
+        X = X.drop(self.column)
         return X.to_pandas()
 
 
 transformer = ColumnTransformer(
     transformers=[
         ("date_transformation", DateTransformer(column="Date"), ["Date"]),
-        ("border_encoder", OneHotEncoder(), ["Border"]),
-        ("state_encoding", OneHotEncoder(), ["State"]),
+        # ("border_encoder", OneHotEncoder(), ["Border"]),
+        # ("state_encoding", OneHotEncoder(), ["State"]),
     ]
 )
 
