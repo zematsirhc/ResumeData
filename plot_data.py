81673dc7ec174c60bab3747c551b003598c5928b
zematsirhc <123906157+zematsirhc@users.noreply.github.com>
2025-01-12 16:56:01 -0600

    Commits Jan 12th. Redoing Main.py and plotdata.py

--- plot_data.py
+++ plot_data.py
@@ -1,31 +1,14 @@
 import matplotlib.pyplot as plt
 import polars as pl
-from sklearn.preprocessing import (
-    MinMaxScaler,
-)  # Transform the value column into a normalized 0-1
 
 train_set = pl.read_parquet(r"border_crossings\data\train_set.parquet")
 
 # Group the data
 train_set = train_set.group_by(["Latitude", "Longitude"]).agg(pl.col("Value").sum())
 
-# Instantiate a scaler so that we can utilize the value as the alpha
-scaler = MinMaxScaler()
-
-# Scale the value column
-scaled_values = scaler.fit_transform(train_set["Value"].to_numpy().reshape(-1, 1))
-
-# Add scaled values back to the DataFrame, adjusting slightly for alpha
-adjusted_alpha = scaled_values.flatten()
-adjustment = 1e-6
-adjusted_alpha = adjusted_alpha * (1 - 2 * adjustment) + adjustment
-
-train_set = train_set.with_columns(pl.Series("Value", adjusted_alpha))
-print(train_set)
-
 # Examine the data by latitute and longitude
 fig, ax = plt.subplots()
-ax.scatter(x=train_set["Longitude"], y=train_set["Latitude"], alpha=train_set["Value"])
+ax.scatter(x=train_set["Longitude"], y=train_set["Latitude"], alpha=0.1)
 
 
 ax.set_title("Border Crossing Locations by Latitude and Longitude")
