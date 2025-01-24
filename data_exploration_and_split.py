07b36a7ef6078fa39b08193bfc31d4a82ee05f83
zematsirhc <123906157+zematsirhc@users.noreply.github.com>
2025-01-24 14:44:37 -0600

    Fully integrate governmental control features

--- data_exploration_and_split.py
+++ data_exploration_and_split.py
@@ -1,14 +1,67 @@
 import polars as pl
 from sklearn.model_selection import ShuffleSplit
+import pandas as pd
 
 border_crossings = pl.read_csv(
     r"C:\Users\Chris\Downloads\Border_Crossing_Entry_Data.csv"
 )
-border_crossings = border_crossings.with_row_index()
+print(border_crossings)
+# Read pickle file with pandas
+government_control = pd.read_pickle(
+    r"border_crossings\data\government_control_processed.pkl"
+)
 
-# Allows us to see all the columns
-with pl.Config(tbl_cols=-1):
-    print(border_crossings)
+# Convert to Polars DF
+government_control = pl.from_pandas(government_control)
+
+# Change years to ints
+government_control = government_control.with_columns(
+    government_control["Start Year"].cast(pl.Int64).alias("Start Year"),
+    government_control["End Year"].cast(pl.Int64).alias("End Year"),
+)
+
+# Add the middle year
+government_control = government_control.with_columns(
+    (pl.col("Start Year") + 1).alias("Middle Year")
+)
+
+# Make sure our assumptions about the year columns are accurate
+assert ((government_control["End Year"] - government_control["Middle Year"]) == 1).all()
+assert (
+    (government_control["Middle Year"] - government_control["Start Year"]) == 1
+).all()
+
+# Get a year column which matches (int64) in the border crossing data set
+border_crossings = border_crossings.with_columns(
+    (pl.col("Date").str.tail(4)).cast(pl.Int64).alias("Year")
+)
+
+# Unpivot the dates so that they're in a single column
+government_control = government_control.unpivot(
+    on=["Start Year", "Middle Year", "End Year"],
+    index=["President Control", "House Control", "Senate Control"],
+    variable_name="Year Type",
+    value_name="Year",
+)
+
+# Ensure there are no duplicate years
+government_control = government_control.unique("Year")
+
+government_control = government_control.sort("Year")
+
+# Join on the year column
+border_crossings = border_crossings.join(
+    government_control,
+    left_on="Year",
+    right_on="Year",
+    how="left",
+)
+
+# Drop extra columns
+border_crossings = border_crossings.drop(["Year Type", "Year"])
+
+
+border_crossings = border_crossings.with_row_index()
 
 # Reserve 20% of the data for testing purposes
 split = ShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
@@ -26,9 +79,5 @@ print(f"Test set: {len(test_set)}  Train set: {len(train_set)}")
 
 print(train_set.describe())
 
-""" train_set.write_parquet(
-    r"border_crossings\data\train_set.parquet"
-)
-test_set.write_parquet(
-    r"border_crossings\data\test_set.parquet"
-) """
+# train_set.write_parquet(r"border_crossings\data\train_set.parquet")
+# test_set.write_parquet(r"border_crossings\data\test_set.parquet")
